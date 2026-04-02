5fe5b8b73a737d2c05513676f60f6cb434ee3f6f
Nataly Jaya Salazar <163929411+NatalyJaya@users.noreply.github.com>
2026-04-02 20:30:45 +0200

    refractor: intial base without the genre structure

--- app/services.py
+++ app/services.py
@@ -25,34 +25,13 @@ def get_local_movies(url, api_key): #de una "plataforma" obtengo sus peliculas
         print(f"Error connecting to API: {e}")
         return []
 
-def get_all_movies(): #obtener todas las peliculas de todas las "plataformas"
+def get_all_movies():  # obtener todas las peliculas de todas las "plataformas"
     movies = []
     for url, key in [
         (url_local_1, api_key_local_1),
         (url_local_2, api_key_local_2),
-        (url_local_3, api_key_local_3),
+        (url_local_3, api_key_local_3)
     ]:
         movies.extend(get_local_movies(url, key))
-
-        #gestionar los generos de las peliculas
-        genres = get_genre(url, key) 
-        for g in genres:
-            genre_map[g["id"]] = g["name"]
-        for movie in movies:
-            gendre_id = movie.get("genre")
-            movie["genre_name"] = genre_map.get(genre_id, "Unknown")
-
+    
     return movies
-
-def get_genre(url, api_key): #
-    try:
-        response = requests.get(
-            f"{url}/genres",
-            headers={"X-API-KEY": api_key},
-            timeout=5
-        )
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error connecting to API: {e}")
-        return []
