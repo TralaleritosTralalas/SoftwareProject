10fc46fad58a1a1d9f59873bf87a5794c8c84367
Nataly Jaya Salazar <163929411+NatalyJaya@users.noreply.github.com>
2026-04-02 21:33:38 +0200

    feat: tag for platform and genre map

--- app/services.py
+++ app/services.py
@@ -11,6 +11,14 @@ api_key_local_1 = config("API_KEY_LOCAL_1")
 api_key_local_2 = config("API_KEY_LOCAL_2")
 api_key_local_3 = config("API_KEY_LOCAL_3")
 
+#PLATAFORMAS
+PLATFORMS = [
+    (url_local_1, api_key_local_1, "Platform 1"),
+    (url_local_2, api_key_local_2, "Platform 2"),
+    (url_local_3, api_key_local_3, "Platform 3"),
+]
+
+
 def get_local_movies(url, api_key): #de una "plataforma" obtengo sus peliculas
     try:
         response = requests.get(
@@ -25,13 +33,38 @@ def get_local_movies(url, api_key): #de una "plataforma" obtengo sus peliculas
         print(f"Error connecting to API: {e}")
         return []
 
+def get_genre(url, api_key):
+    try:
+        response = requests.get(
+            f"{url}/genres", 
+            headers={"X-API-KEY": api_key}, 
+            timeout=5
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error connecting to API: {e}")
+        return []
+
+
 def get_all_movies():  # obtener todas las peliculas de todas las "plataformas"
-    movies = []
-    for url, key in [
-        (url_local_1, api_key_local_1),
-        (url_local_2, api_key_local_2),
-        (url_local_3, api_key_local_3)
-    ]:
-        movies.extend(get_local_movies(url, key))
+    all_movies = []
+    for url, key, platform_name in PLATFORMS:
+        
+        # mapa de generos x plataforma
+        genre_map = {}
+
+        genres = get_genre(url, key)
+        for g in genres:
+            genre_map[g["id"]] = g["name"]
+
+        movies = get_local_movies(url, key)
+        for movie in movies:
+            genre_id = movie.get("genre_id") #tenemos el id que se asocia con su correspondente genero en string
+            movie["genre_name"] = genre_map.get(genre_id, "Unknown")
+            movie["platform_name"] = platform_name
     
-    return movies
+        all_movies.extend(movies)
+    
+    return all_movies
+
