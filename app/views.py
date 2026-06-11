3aadf36d0aa7464a90fdc4418937ad04f7eda58c
Nataly Jaya Salazar <163929411+NatalyJaya@users.noreply.github.com>
2026-06-11 04:36:21 +0200

    fix(navbar): adapt changes

--- app/views.py
+++ app/views.py
@@ -347,42 +347,6 @@ def content_detail(request, ctype, cid):
     
     content = None
 
-        user_status = 'not_seen'
-        is_favorite = False
-        is_in_watchlist = False
-
-        if request.user.is_authenticated:
-            try:
-                if ctype == 'series':
-                    local_content = Series.objects.filter(title=content.get('title')).first()
-                else:
-                    local_content = Movie.objects.filter(title=content.get('title')).first()
-
-                if local_content:
-                    vp = VisualizationProgress.objects.filter(user=request.user, content=local_content).first()
-                    if vp:
-                        if vp.completed:
-                            user_status = 'completed'
-                        elif vp.last_minute > 0:
-                            user_status = 'watching'
-
-                    is_favorite = Favorite.objects.filter(user=request.user, content=local_content).exists()
-                    is_in_watchlist = Watchlist.objects.filter(user=request.user, content=local_content).exists()
-
-                    content_in_lists = list(Watchlist.objects.filter(
-                        user=request.user,
-                        content=local_content
-                    ).values_list('id', flat=True))
-            except Exception:
-                pass
-
-        return render(request, 'pages/content_view.html', {
-            'content': content,
-            'user_status': user_status,
-            'is_favorite': is_favorite,
-            'is_in_watchlist': is_in_watchlist,
-            'content_in_lists': content_in_lists if 'content_in_lists' in locals() else []
-        })
     if cid.isdigit():
         content = next((item for item in all_content if item.get('id') == int(cid)), None)
     else:
